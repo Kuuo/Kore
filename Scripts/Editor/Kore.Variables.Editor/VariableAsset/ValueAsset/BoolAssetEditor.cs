f577b8a3b411797cdc2bdb1972274a367c9e3a1c
Kuuo <1098335635@qq.com>
2019-06-11 16:33:00 +0800

    Upadte Variables Asset Editors

---
+++ Scripts/Editor/Kore.Variables.Editor/VariableAsset/ValueAsset/BoolAssetEditor.cs
@@ -0,0 +1,11 @@
+﻿using UnityEditor;
+using Kore.Variables;
+using Kore.Events;
+
+namespace Kore.Variables.Editor
+{
+    [CustomEditor(typeof(BoolAsset))]
+    public class BoolAssetEditor : ValueAssetEditor<bool, BoolAsset, BoolGameEvent>
+    {
+    }
+}
---
+++ Scripts/Editor/Kore.Variables.Editor/VariableAsset/ValueAsset/FloatAssetEditor.cs
@@ -0,0 +1,11 @@
+﻿using UnityEditor;
+using Kore.Variables;
+using Kore.Events;
+
+namespace Kore.Variables.Editor
+{
+    [CustomEditor(typeof(FloatAsset))]
+    public class FloatAssetEditor : ValueAssetEditor<float, FloatAsset, FloatGameEvent>
+    {
+    }
+}
